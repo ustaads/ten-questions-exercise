03783a3aff09b17fb8c508b0abe9ad89d4f4e6c1
Amit Sharma <amitsharma.as741@gmail.com>
2018-04-21 01:42:39 -0400

    Added getStringLength Function

---
+++ getStringLength.js
@@ -0,0 +1,9 @@
+
+let getStringLength = (string) =>{
+    return string.length;
+};
+
+let name = 'Amit Sharma';
+let length = getStringLength(name);
+
+console.log(`${name} has ${length} characters in total.`);
